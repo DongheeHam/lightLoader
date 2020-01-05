1b84a782ec3c39b8cd78cecf211301d26fae67f2
DongheeHam <57735083+DongheeHam@users.noreply.github.com>
2020-01-05 19:49:49 +0900

    Update lightLoader.js
    ver 0.0.2 : add animation (use Jquery)

--- lightLoader/js/lightLoader.js
+++ lightLoader/js/lightLoader.js
@@ -8,22 +8,44 @@ loader.setAttribute("id", "loader");
 
 loaderContainerX.appendChild(loaderContainerY);
 loaderContainerY.appendChild(loader);
-window.onload=function(){
+/*window.onload=function(){
 	//document.body.appendChild(loaderContainerX);
 	document.body.
 	insertBefore(loaderContainerX, document.body.firstChild);
+}*/
+if(jQuery){
+	$(document).ready(function(){
+		document.body.
+		insertBefore(loaderContainerX, document.body.firstChild);
+		$(loaderContainerX).css('visibility','visible');
+		lightLoader.hide();
+	});
+}else{
+	window.addEventListener('DOMContentLoaded', function(){
+		document.body.
+		insertBefore(loaderContainerX, document.body.firstChild);
+		x.style.visibility="visible";
+		lightLoader.hide();
+	});
 }
-
 var lightLoader={
 	show:function(){
 		//$("#l-x").css('display','flex');
 		var x=document.getElementById("loader-container-x");
-		x.style.display="flex";
-		document.body.style.overflow="hidden";
+		if(jQuery){
+			$(x).fadeTo(400,1);
+		}else{
+			x.style.display="flex";
+			document.body.style.overflow="hidden";
+		}
 	},
 	hide:function(){
 		//$("#l-x").css('display','none');
 		var x=document.getElementById("loader-container-x");
-		x.style.display="none";
+		if(jQuery){
+			$(x).fadeOut(400);
+		}else{
+			x.style.display="none";
+		}
 	}
 }
